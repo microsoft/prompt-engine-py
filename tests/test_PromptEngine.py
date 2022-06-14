93f329ee9315e5258a81f99439468ca021c12459
Abhishek Masand <amasand@microsoft.com>
2022-06-14 15:11:52 +0530

    Added tests

--- tests/test_PromptEngine.py
+++ tests/test_PromptEngine.py
@@ -2,7 +2,7 @@ from src.prompt_refresher.promptEngine import PromptEngine, PromptEngineConfig
 from src.prompt_refresher.modelConfig import ModelConfig
 from src.prompt_refresher.interaction import Interaction
 
-def test_pass_1():
+def test_pass():
     config = PromptEngineConfig(ModelConfig(max_tokens=32), commentOperator = "###")
     description = "This code takes in natural language utterance and generates code This code takes in natural language utterance and generates code"
     examples = [Interaction("Hello", "print('Hello')"), Interaction("Goodbye", "print('Goodbye')")]
@@ -10,14 +10,20 @@ def test_pass_1():
     promptEngine = PromptEngine(config, description, examples, dialog)
     assert promptEngine.buildContext() != ""
 
-def test_pass_2():
+def test_pass_model_config():
     config = PromptEngineConfig(ModelConfig(max_tokens=32), commentOperator = "###")
     description = ""
     promptEngine = PromptEngine(config, description)
     assert promptEngine.buildContext() != ""
 
 
-def test_pass_3():
+def test_pass_no_paramters():
+    config = PromptEngineConfig()
+    description = ""
+    promptEngine = PromptEngine(config, description)
+    assert promptEngine.buildContext() == ""
+
+def test_pass_removeLastInteraction():
     config = PromptEngineConfig()
     description = ""
     dialog = [Interaction("Hi", "print('Hi')"), Interaction("Bye", "print('Bye')")]
@@ -26,10 +32,40 @@ def test_pass_3():
     promptEngine.removeLastInteraction()
     assert promptEngine.buildContext() == "## Hi\nprint('Hi')\n"
 
-
-
-def test_fail_1():
+def test_fail_removeLastInteraction():
     config = PromptEngineConfig()
     description = ""
     promptEngine = PromptEngine(config, description)
-    assert promptEngine.buildContext() == ""
+    promptEngine.buildContext()
+    try:
+        promptEngine.removeLastInteraction()
+    except Exception as e:
+        assert str(e) == "No interactions to remove"
+
+def test_pass_removeFirstInteraction():
+    config = PromptEngineConfig()
+    description = ""
+    dialog = [Interaction("Hi", "print('Hi')"), Interaction("Bye", "print('Bye')")]
+    promptEngine = PromptEngine(config, description, dialog=dialog)
+    promptEngine.buildContext()
+    promptEngine.removeFirstInteraction()
+    assert promptEngine.buildContext() == "## Bye\nprint('Bye')\n"
+
+def test_fail_removeFirstInteraction():
+    config = PromptEngineConfig()
+    description = ""
+    promptEngine = PromptEngine(config, description)
+    promptEngine.buildContext()
+    try:
+        promptEngine.removeFirstInteraction()
+    except Exception as e:
+        assert str(e) == "No interactions to remove"
+
+def test_pass_addInteraction():
+    config = PromptEngineConfig()
+    description = ""
+    dialog = [Interaction("Hi", "print('Hi')")]
+    promptEngine = PromptEngine(config, description, dialog=dialog)
+    promptEngine.buildContext()
+    promptEngine.addInteraction(Interaction("Bye", "print('Bye')"))
+    assert promptEngine.buildContext() == "## Hi\nprint('Hi')\n## Bye\nprint('Bye')\n"
