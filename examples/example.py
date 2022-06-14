f95878e3d4ee8b5151841476bad88684675f93f9
Abhishek Masand <amasand@microsoft.com>
2022-06-14 16:03:29 +0530

    Update example

--- examples/example.py
+++ examples/example.py
@@ -2,7 +2,7 @@ from prompt_refresher.promptEngine import PromptEngine, PromptEngineConfig
 from prompt_refresher.modelConfig import ModelConfig
 from prompt_refresher.interaction import Interaction
 
-config = PromptEngineConfig(ModelConfig(max_tokens=32), commentOperator = "###")
+config = PromptEngineConfig(ModelConfig(max_tokens=50), commentOperator = "###")
 description = "This code takes in natural language utterance and generates code This code takes in natural language utterance and generates code"
 examples = [Interaction("Hello", "print('Hello')"), Interaction("Goodbye", "print('Goodbye')")]
 dialog = [Interaction("Hi", "print('Hi')"), Interaction("Bye", "print('Bye')")]
