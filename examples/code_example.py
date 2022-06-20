eaa1a915796e42561ff221d2450133d85413c940
Abhishek Masand <amasand@microsoft.com>
2022-06-21 02:38:51 +0530

    Add additonal configs for Python and Javascript, added a CodeEngine example

---
+++ examples/code_example.py
@@ -0,0 +1,11 @@
+from prompt_engine.code_engine import CodeEngine, PythonCodeEngineConfig
+from prompt_engine.model_config import ModelConfig
+from prompt_engine.interaction import Interaction
+
+config = PythonCodeEngineConfig(ModelConfig(max_tokens=50))
+description = "This code takes in natural language utterance and generates code This code takes in natural language utterance and generates code"
+examples = [Interaction("Hello", "print('Hello')"), Interaction("Goodbye", "print('Goodbye')")]
+interactions = [Interaction("Hi", "print('Hi')"), Interaction("Bye", "print('Bye')")]
+code_engine = CodeEngine(config=config, description=description, examples=examples, interactions=interactions)
+
+print (code_engine.build_context())
--- src/prompt_engine/code_engine.py
+++ src/prompt_engine/code_engine.py
@@ -3,6 +3,12 @@ from prompt_engine.model_config import ModelConfig
 from prompt_engine.interaction import Interaction
 
 class CodeEngineConfig(PromptEngineConfig):
+    def __init__(self, model_config: ModelConfig = None, description_prefix: str = "### ", description_postfix: str = "###", newline_operator: str = "\n",
+                 input_prefix: str = "##", input_postfix: str = "", output_prefix: str = "", output_postfix: str = ""):
+        super().__init__(model_config = model_config, description_prefix = description_prefix, description_postfix = description_postfix, newline_operator = newline_operator,
+                                input_prefix = input_prefix, input_postfix = input_postfix, output_prefix = output_prefix, output_postfix = output_postfix)
+
+class JavaScriptCodeEngineConfig(CodeEngineConfig):
     """
     This class provides the configuration for the Code Engine
     """
@@ -10,6 +16,16 @@ class CodeEngineConfig(PromptEngineConfig):
                  input_prefix: str = "/*", input_postfix: str = "*/", output_prefix: str = "", output_postfix: str = ""):
         super().__init__(model_config = model_config, description_prefix = description_prefix, description_postfix = description_postfix, newline_operator = newline_operator,
                                 input_prefix = input_prefix, input_postfix = input_postfix, output_prefix = output_prefix, output_postfix = output_postfix)
+
+class PythonCodeEngineConfig(CodeEngineConfig):
+    """
+    This class provides the configuration for the Code Engine
+    """
+    def __init__(self, model_config: ModelConfig = None, description_prefix: str = "### ", description_postfix: str = "###", newline_operator: str = "\n",
+                 input_prefix: str = "##", input_postfix: str = "", output_prefix: str = "", output_postfix: str = ""):
+        super().__init__(model_config = model_config, description_prefix = description_prefix, description_postfix = description_postfix, newline_operator = newline_operator,
+                                input_prefix = input_prefix, input_postfix = input_postfix, output_prefix = output_prefix, output_postfix = output_postfix)
+
 
 class CodeEngine(PromptEngine):
     """
