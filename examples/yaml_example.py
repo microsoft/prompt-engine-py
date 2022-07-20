8983811ae6f33a07c30ffff78886254b6eb57f98
Abhishek Masand <amasand@microsoft.com>
2022-07-21 01:55:45 +0530

    Updated example

--- examples/yaml_example.py
+++ examples/yaml_example.py
@@ -9,18 +9,18 @@ Output of this example is:
 
 >> What is the possibility of an event happening? !
 
-> Roam around Mars
-This will be possible in a couple years
+Human: Roam around Mars
+Bot: This will be possible in a couple years
 
-> Drive a car
-This is possible after you get a learner drivers license
+Human: Drive a car
+Bot: This is possible after you get a learner drivers license
 
 >> Starting a new conversation !
 
-> Drink water
-Uhm...You don't do that 8 times a day?
+Human: Drink water
+Bot: Uhm...You don't do that 8 times a day?
 
-> Walk on air
-For that you'll need a special device
+Human: Walk on air
+Bot: For that you'll need a special device
 
 """
