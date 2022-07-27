645bbb3dbf33d1090e88ff28fb439686a4fc456c
Abhishek Masand <amasand@microsoft.com>
2022-07-28 01:32:51 +0530

    Added kwargs update in model config when loading via yaml

--- src/prompt_engine/model_config.py
+++ src/prompt_engine/model_config.py
@@ -2,5 +2,6 @@ class ModelConfig:
     """
     Interaction class is used to store the model config to be used in the prompt engine
     """
-    def __init__(self, max_tokens):
+    def __init__(self, max_tokens, **kwargs):
         self.max_tokens = max_tokens
+        self.__dict__.update(kwargs)
